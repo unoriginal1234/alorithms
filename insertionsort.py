72a4809280d53fc59881b8415608ec60e655b012
unoriginal1234 <eric.t.roth@gmail.com>
2023-07-22 21:40:10 -0700

    Algorithm works

--- insertionsort.py
+++ insertionsort.py
@@ -1,1 +1,24 @@
-# Doing insertion sort
+# Insertion Sort
+"""
+Pseudocode:
+for i = 2 to n
+    key = A[i]
+    # Insert A[i] into the sorted subarray A[1: i -1]
+    j = i - 1
+    while j > 0 and A[j] > key
+        A[j + 1] = A[j]
+        j = j - 1
+    A[j + 1 ] = key
+"""
+A = [5, 2, 4, 6, 1, 3]
+n = len(A)
+
+for i in range(1, n):
+    key = A[i]
+    j = i - 1
+    while j >= 0 and A[j] > key:
+        A[j + 1] = A[j]
+        j = j - 1
+    A[j + 1] = key
+
+print(A)
