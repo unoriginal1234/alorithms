d5f7de7fa32e4f5ac9e6d4160dc52f55450d6ca9
unoriginal1234 <eric.t.roth@gmail.com>
2023-07-24 19:15:29 -0700

    added Add Binary Integers

---
+++ addbinaryintegers.py
@@ -0,0 +1,12 @@
+"""
+Consider the problem of adding two n-bit binary integers a and b, 
+stored in two n-element arrays A[0:n-1] and B[0:n-1],
+where each element is either 0 or 1, 
+a = the SUMMATION of A[i]*2**i and b = the SUMMATION of B[i]*2**i.
+
+The sum c = a + b of the two integers should be stored in binary form in an
+(n+1)-element array C[0:n], where c = the SUMMATION of C[i]*2**i.
+
+Write a procedure ADD-BINARY-INTEGERS that takes as input arrays A and B, 
+along with the length n and returns array C holding the sum"""
+
--- insertionsort.py
+++ insertionsort.py
@@ -10,13 +10,29 @@ for i = 2 to n
         j = j - 1
     A[j + 1 ] = key
 """
-A = [5, 2, 4, 6, 1, 3]
+
+# Execution
+# A = [31, 41,59, 26, 41, 58]
+# n = len(A)
+
+# for i in range(1, n):
+#     key = A[i]
+#     j = i - 1
+#     while j >= 0 and A[j] > key:
+#         A[j + 1] = A[j]
+#         j = j - 1
+#     A[j + 1] = key
+
+# print(A)
+
+# Now in reverse
+A = [31, 41,59, 26, 41, 58]
 n = len(A)
 
 for i in range(1, n):
     key = A[i]
     j = i - 1
-    while j >= 0 and A[j] > key:
+    while j >= 0 and A[j] < key:
         A[j + 1] = A[j]
         j = j - 1
     A[j + 1] = key
